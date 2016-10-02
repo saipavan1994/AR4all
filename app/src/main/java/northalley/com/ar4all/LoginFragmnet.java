b07ff034716be1e7b1eddc14d3e06496016291de
saipavan1994 <saipavan1994@gmail.com>
2016-10-02 18:31:24 +0530

    Drawable screen

--- app/src/main/java/northalley/com/ar4all/LoginFragmnet.java
+++ app/src/main/java/northalley/com/ar4all/LoginFragmnet.java
@@ -13,7 +13,7 @@ import android.widget.TextView;
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
  *
- * Use the {@link LoginFragmnet#newInstance} factory method to
+ * Use the {@link LoginFragmnet#//newInstance} factory method to
  * create an instance of this fragment.
  */
 public class LoginFragmnet extends Fragment {
@@ -36,8 +36,8 @@ public class LoginFragmnet extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param ///param1 Parameter 1.
+     * @param //param2 Parameter 2.
      * @return A new instance of fragment LoginFragmnet.
      */
     // TODO: Rename and change types and number of parameters
--- app/src/main/java/northalley/com/ar4all/UserLoginReg.java
+++ app/src/main/java/northalley/com/ar4all/UserLoginReg.java
@@ -1,9 +1,12 @@
 package northalley.com.ar4all;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class UserLoginReg extends FragmentActivity{
 
@@ -19,6 +22,8 @@ public class UserLoginReg extends FragmentActivity{
         login.setArguments(getIntent().getExtras());
         /*Fragment is loaded into the container*/
         getSupportFragmentManager().beginTransaction().add(R.id.user_reg_login,login).commit();
+      final EditText  pwd = (EditText)findViewById(R.id.password_text);
+      final EditText  eml =(EditText)findViewById(R.id.emailid_text);
 
     }
     public void goToReg(View v)
@@ -27,12 +32,35 @@ public class UserLoginReg extends FragmentActivity{
         register.setArguments(getIntent().getExtras());
         getSupportFragmentManager().beginTransaction().replace(R.id.user_reg_login,register).addToBackStack(null).commit();
 
+
     }
     public  void goToLog(View v)
     {
-        LoginFragmnet login = new LoginFragmnet();
-        login.setArguments(getIntent().getExtras());
-        getSupportFragmentManager().beginTransaction().replace(R.id.user_reg_login,login).commit();
+        final EditText usrnm = (EditText)findViewById(R.id.username_text);
+        final EditText eml_reg =(EditText)findViewById(R.id.email_id_text);
+        final EditText pwd_reg=(EditText)findViewById(R.id.password_reg_text);
+        final EditText re_pwd_reg=(EditText)findViewById(R.id.renetr_password_text);
+        final EditText ph_num=(EditText)findViewById(R.id.phone_number_text);
+        String username= usrnm.getText().toString();
+        String email =eml_reg.getText().toString();
+        String password=pwd_reg.getText().toString();
+        String reenter_password=re_pwd_reg.getText().toString();
+        String phone= ph_num.getText().toString();
+        if(username.isEmpty()||email.isEmpty()||password.isEmpty()||reenter_password.isEmpty()||phone.isEmpty())
+        {
+            Toast.makeText(getApplicationContext(),"Please Make Sure You Have Entered All Fields",Toast.LENGTH_SHORT).show();
+        }
+
+        else {
+            LoginFragmnet login = new LoginFragmnet();
+            login.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction().replace(R.id.user_reg_login, login).commit();
+        }
+    }
+    public void goToMain(View v)
+    {
+        Intent intent = new Intent(this,DrawerActivity.class);
+        startActivity(intent);
     }
 
 }
